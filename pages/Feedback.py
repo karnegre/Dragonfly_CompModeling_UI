a2e312658bbcb775836609f573e7cb785f6f46dc
Negrete <karla.negrete@jhuapl.edu>
2021-11-19 09:50:52 -0500

    feedback

--- pages/Feedback.py
+++ pages/Feedback.py
@@ -5,10 +5,12 @@ def app():
 
     st.header("Beta testers, provide feedback here!")
 
-    contact_form = """ <form action="https://formsubmit.co/Karla.Negrete@jhuapl.edu" method="POST">
-     <input type="text" name="name" required>
-     <input type="email" name="email" required>
-     <button type="submit">Send</button>
-     </form>"""
+    contact_form = """
+    <form action="https://formsubmit.co/Karla.Negrete@jhuapl.edu" method="POST">
+         <input type="text" name="name" required>
+         <input type="email" name="email" required>
+         <button type="submit">Send</button>
+     </form>
+     """
 
-    st.markdown(contact_form, unsafe_allow html = True)
+    st.markdown(contact_form, unsafe_allow_html = True)
