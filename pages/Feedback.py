875ee4daca49504ff227921a1c2371ebc9937277
Negrete <karla.negrete@jhuapl.edu>
2021-11-19 09:40:54 -0500

    feedback

--- pages/Feedback.py
+++ pages/Feedback.py
@@ -5,7 +5,7 @@ def app():
 
     st.header("Beta testers, provide feedback here!")
 
-    contact_form = "" <form action="https://formsubmit.co/your@email.com" method="POST">
+    contact_form = "" <form action="https://formsubmit.co/Karla.Negrete@jhuapl.edu" method="POST">
      <input type="text" name="name" required>
      <input type="email" name="email" required>
      <button type="submit">Send</button>
